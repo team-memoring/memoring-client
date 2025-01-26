22808dc38a503d7d96c449c8823a6e080a08aaf3
kyuhho <rbgh0114@naver.com>
2025-01-26 23:35:43 +0900

    feat: add svg config

--- App.tsx
+++ App.tsx
@@ -1,39 +1,30 @@
 import React from 'react';
 
-import {createStaticNavigation} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import LoginScreen from './src/screens/LoginScreen';
-import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
-import {StyleSheet} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import LoginScreen from 'screens/LoginScreen';
 
-const RootStack = createNativeStackNavigator({
-  screens: {
-    Login: {
-      screen: LoginScreen,
-      options: {
-        headerShown: false,
-      },
-    },
-  },
-});
-
-const Navigation = createStaticNavigation(RootStack);
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Navigation />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{
+              headerShown: false,
+              contentStyle: {backgroundColor: '#f9ebe4'},
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
