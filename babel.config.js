d9146755590b75db8225825036c52e66059c25e5
kyuhho <rbgh0114@naver.com>
2025-01-26 23:35:54 +0900

    feat: set baseUrl config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './',
+          // '@const': './src/const',
+        },
+      },
+    ],
+  ],
 };
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import {View} from 'react-native';
-import CustomText from '../components/shared/CustomText';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+// import Logo from './logo.svg';
 
-const LoginScreen = () => {
+const LoginScreen = (): React.JSX.Element => {
   return (
-    <View>
-      <CustomText>로그인 화면</CustomText>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        translucent
+        backgroundColor="#f9ebe4"
+        barStyle="dark-content"
+      />
+      <View style={styles.content}>
+        {/* <Logo width={100} height={100} /> */}
+        <Text>Login Screen</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f9ebe4',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default LoginScreen;
