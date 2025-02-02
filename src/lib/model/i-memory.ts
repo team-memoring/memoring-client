a655e2eebb0da98b1c7d0054a697625589c1b773
kyuhho <rbgh0114@naver.com>
2025-02-02 21:25:00 +0900

    chore: add member interface

--- src/lib/model/i-memory.ts
+++ src/lib/model/i-memory.ts
@@ -1,5 +1,7 @@
 // This file is temporary. It will be deleted when the actual API is connected.
 
+import {FamilyRole} from './i-family';
+
 export enum QuizStatus {
   NOT_STARTED = '풀이 전',
   IN_PROGRESS = '풀이 중',
@@ -14,4 +16,15 @@ export interface IMemory {
   solvedQuizCount: number;
   createdAt: Date;
   status: QuizStatus;
+}
+
+export interface IEventRegister {
+  date: Date;
+  images: string[]; // URI or base64
+  description: string;
+}
+export interface IMemoryRegister {
+  title: string;
+  roles: FamilyRole[];
+  events: IEventRegister[];
 }
