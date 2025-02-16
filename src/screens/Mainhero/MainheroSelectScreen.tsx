f417cc6e0102f22e255060aa57697c122431786a
MartinJ-31 <rjdls07@naver.com>
2025-02-16 17:01:33 +0900

    feat:add functions for navigation.

--- src/screens/Mainhero/MainheroSelectScreen.tsx
+++ src/screens/Mainhero/MainheroSelectScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import Header from '../../components/shared/Header';
@@ -11,12 +11,9 @@ import {getUser} from '../../utils/storage';
 
 import MainheroView from '../../components/Mainhero/MainheroView';
 
-const categories = ['가족 여행', '취미 활동', '직장 생활', '어린 시절'];
-
 const MainheroSelectScreen = (): React.JSX.Element => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   const [username, setUsername] = React.useState<string | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   useEffect(() => {
     const loadUsername = async () => {
@@ -47,48 +44,11 @@ const MainheroSelectScreen = (): React.JSX.Element => {
           </CustomText>
           <CustomText
             weight="ExtraBold"
-            style={{fontSize: 25, color: '#222225'}}>
+            style={{fontSize: 25, color: '#222225', marginBottom: 20}}>
             어떤 추억을 떠올려볼까요?
           </CustomText>
         </View>
-        <View
-          style={{
-            paddingVertical: 20,
-          }}>
-          <FlatList
-            data={categories}
-            keyExtractor={item => item}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            renderItem={({item, index}) => {
-              const isSelected = selectedCategory === item;
-
-              const isFirstItem = index === 0;
-              const isLastItem = index === categories.length - 1;
-
-              return (
-                <TouchableOpacity
-                  style={[
-                    styles.categoryItem,
-                    isSelected && styles.selectedCategory,
-                    {marginRight: !isLastItem ? 8 : 16},
-                    {marginLeft: isFirstItem ? 16 : 0},
-                  ]}
-                  onPress={() => setSelectedCategory(item)}>
-                  <CustomText
-                    weight={isSelected ? 'ExtraBold' : 'Bold'}
-                    style={[
-                      {fontSize: 16, color: '#CE541A'},
-                      isSelected && {fontSize: 16, color: '#FFFFFF'},
-                    ]}>
-                    {item}
-                  </CustomText>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </View>
-        <MainheroView selectedCategory={selectedCategory} />
+        <MainheroView />
       </View>
     </SafeAreaView>
   );
@@ -102,16 +62,6 @@ const styles = StyleSheet.create({
   text: {
     paddingTop: 12,
     paddingHorizontal: 16,
-  },
-  categoryItem: {
-    backgroundColor: '#F5D9CC',
-    borderRadius: 48.5,
-
-    paddingVertical: 12,
-    paddingHorizontal: 14,
-  },
-  selectedCategory: {
-    backgroundColor: '#CE541A',
   },
 });
 
