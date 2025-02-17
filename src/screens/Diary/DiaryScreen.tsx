8e6769c285045604f160a0a834424ff0d0bf9e5e
MartinJ-31 <rjdls07@naver.com>
2025-02-17 13:20:28 +0900

    feat:add title header.

--- src/screens/Diary/DiaryScreen.tsx
+++ src/screens/Diary/DiaryScreen.tsx
@@ -3,7 +3,7 @@ import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
-import {BackHeader, CustomText} from '../../components/shared';
+import {TitleHeader, CustomText} from '../../components/shared';
 import {ParamListBase, useNavigation} from '@react-navigation/native';
 import DiaryContentsView from '../../components/Diary/DiaryContentsView';
 
@@ -12,27 +12,22 @@ const DiaryScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{paddingVertical: 10}}>
-        <BackHeader onBackPress={() => navigation.navigate('MainheroSelect')} />
+      <View style={{flexDirection: 'row', paddingVertical: 10}}>
+        <TitleHeader
+          onBackPress={() => navigation.navigate('MainheroSelect')}
+          title="가족 다이어리"
+          color="#CE5419"
+        />
       </View>
-      <View style={{paddingTop: 12, paddingBottom: 0, paddingHorizontal: 16}}>
+      <View style={{paddingVertical: 12, paddingHorizontal: 16}}>
         <CustomText
           weight="ExtraBold"
           style={{
-            fontSize: 25,
+            fontSize: 24,
             color: '#222225',
             lineHeight: 32,
           }}>
-          우리 가족 다이어리를
-        </CustomText>
-        <CustomText
-          weight="ExtraBold"
-          style={{
-            fontSize: 25,
-            color: '#222225',
-            lineHeight: 32,
-          }}>
-          함께 확인해보아요.
+          우리 가족 다이어리를{'\n'}함께 확인해보아요.
         </CustomText>
       </View>
       <View style={{paddingTop: 20}}>
