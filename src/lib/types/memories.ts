223c891ee341c2892534a2ce362b9df9ce4931a5
kyuhho <rbgh0114@naver.com>
2025-02-20 15:45:45 +0900

    feat: add response type

--- src/lib/types/memories.ts
+++ src/lib/types/memories.ts
@@ -1,3 +1,13 @@
 export interface PostMemoriesResponse {
   memoryId: number;
 }
+
+export interface GetMemoriesMembersResponseElement {
+  memoryId: number;
+  memoryTitle: string;
+  memoryUploadTime: string;
+  totalQuizzes: number;
+  completedQuizzes: number;
+}
+
+export type GetMemoriesMembersResponse = GetMemoriesMembersResponseElement[];
