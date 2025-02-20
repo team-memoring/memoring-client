7a68e63218faf347a435f9d4f13317451a11bd46
MartinJ-31 <rjdls07@naver.com>
2025-02-20 19:56:59 +0900

    fix:add quiz generate llm model api.

--- src/screens/Member/MemberQuizGenScreen.tsx
+++ src/screens/Member/MemberQuizGenScreen.tsx
@@ -5,27 +5,62 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {ParamListBase, useNavigation} from '@react-navigation/native';
 
 import Logo from '../../assets/icons/logo.svg';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import MemberLoadingCharacter from '../../components/Member/QuizGen/MemberLoadingCharacter';
+
+import {getQuizzes} from '../../api/memoring/quizzes';
+import {getEvents} from '../../api/memoring/events';
+import {QuizPair} from '../../lib/types/quizzes';
+
+type QuizzesResponse = QuizPair[];
 
 const MemberQuizGenScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
-
+  const [events, setEvents] = useState([]);
+  const [memory_id, setMemory_id] = useState(0);
+  const [memories_length, setMemories_length] = useState(0);
   const [isCreating, setIsCreating] = useState(false);
 
   const isCreateDisabled = isCreating;
 
   const createRequest = async () => {
-    // TODO: API 호출 처리
-    setTimeout(() => {
-      navigation.navigate('MemberQuizList');
-    }, 8000);
+    try {
+      const quizzes = await getQuizzes(memory_id);
+      const quizzesData: QuizzesResponse = quizzes.data;
+      console.log('quizzes on gen:', quizzesData);
+
+      if (quizzes) {
+        navigation.navigate('MemberQuizList', {data: quizzesData});
+      }
+    } catch (error) {
+      console.error('Error fetching quizzes:', error);
+    }
   };
 
   const handelQuizCreate = async () => {
     setIsCreating(true);
     await createRequest();
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const events = await getEvents();
+        if (events?.data) {
+          setEvents(events.data);
+          setMemory_id(events.data[0].memory_id);
+          setMemories_length(events.data.length);
+        } else {
+          setEvents([]);
+          console.log('No events');
+        }
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -46,7 +81,7 @@ const MemberQuizGenScreen = () => {
         <CustomText
           weight="ExtraBold"
           style={{fontSize: 28, marginTop: 8, color: '#222225'}}>
-          2개의 이벤트로
+          {memories_length}개의 이벤트로
         </CustomText>
         <CustomText weight="ExtraBold" style={{fontSize: 28, color: '#222225'}}>
           {isCreating
