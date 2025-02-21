ff8517e4c8d15246c12a776ae7b1e96ca5436e1c
MartinJ-31 <rjdls07@naver.com>
2025-02-21 13:17:18 +0900

    feat:add quiz api.

--- src/api/memoring/quizzes.ts
+++ src/api/memoring/quizzes.ts
@@ -1,3 +1,4 @@
+import QuizDetailCard from '../../components/Member/QuizDetail/QuizDetailCard';
 import apiClient from './apiClient';
 
 export const getQuizzes = async (memoryId: number) => {
@@ -6,4 +7,15 @@ export const getQuizzes = async (memoryId: number) => {
 
 export const postQuizzes = async (quizData: any) => {
   return apiClient.post(`/api/v1/quizzes/quizupload`, quizData);
+};
+
+export const getQuizzesShowquizMemoryId = async (memoryId: number) => {
+  return apiClient.get(`/api/v1/quizzes/showquiz/${memoryId}`);
+};
+
+export const patchQuizzedUpdateQuizId = async (
+  quizId: number,
+  choice: string,
+) => {
+  return apiClient.patch(`/api/v1/quizzes/update/${quizId}?user_ans=${choice}`);
 };
