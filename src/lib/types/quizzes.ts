c5f44e704937d7192b85db5dc41f361f5cb40d30
MartinJ-31 <rjdls07@naver.com>
2025-02-21 13:16:05 +0900

    feat: add quiz type for solving.

--- src/lib/types/quizzes.ts
+++ src/lib/types/quizzes.ts
@@ -26,3 +26,24 @@ export interface QuizStorage {
   memory_id: number;
   family_id: number;
 }
+
+export interface QuizMain {
+  quiz_id: number;
+  quiz_question: string;
+  quiz_choice_1: string;
+  quiz_choice_2: string;
+  quiz_choice_3: string;
+  quiz_answer: string;
+  quiz_img: string;
+  is_dummy: false;
+}
+
+export interface Memory {
+  memory_id: number;
+  quizees: QuizMain[];
+}
+
+export interface QuizDummy {
+  initial_react: string;
+  main_react: string;
+}
